a03bc255df5ecee6e5ceb8ffe9ed43680652abaa
sinan <sinanhd@g>
2023-06-25 19:44:51 +0300

    Add new function to example.py

--- project/settings.py
+++ project/settings.py
@@ -140,3 +140,9 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pythondeveloper6@gmail.com'
+EMAIL_HOST_PASSWORD = 'kcpnwirvkmagqrlr'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
