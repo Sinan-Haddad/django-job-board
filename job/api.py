bcfea5f234dcd666114200aa2e414b6f73cc458d
sinan <sinanhd@g>
2023-06-26 22:24:52 +0300

    Class Based Views

--- job/api.py
+++ job/api.py
@@ -4,7 +4,7 @@
 # from .serializers import JobSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-# from rest_framework import generics
+from rest_framework import generics
 
 
 # @api_view(['GET'])
@@ -17,3 +17,16 @@ def job_list_api(request):
     data = JobSerializer(all_jobs, many=True).data
     return Response({'data':data})
     
+@api_view(['GET'])
+def job_detail_api(request,id):
+    job_detail = Job.objects.get(id=id)
+    data = JobSerializer(job_detail).data
+    return Response({'data':data})
+
+class JobListApi(generics.ListAPIView):
+    model=Job
+    queryset = Job.objects.all()
+    serializer_class=JobSerializer
+
+
+
